aa120f0384fcd183b4e4058c572efb090c20098a
Amit Suthar <sutharamit707@gmail.com>
2025-07-03 23:05:33 +0530

    dashboard template

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ui/ThemedText";
+import { ThemedView } from "@/components/ui/ThemedView";
 
 export default function SearchScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Search</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText>Search</ThemedText>
+    </ThemedView>
   );
 }
 
